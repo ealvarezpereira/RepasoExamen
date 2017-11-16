02d0e0b66b22a1dd0a441afdf593fa8edc7a3d1b
Enrique Alvarez <ealvarezpereira@danielcastelao.org>
2017-11-16 17:53:40 +0100

    Revert "Commit 2. "
    This reverts commit 522baa622e5034ca70e5ed7910b06fb9e577d451.

--- RepasoExamen.java
+++ RepasoExamen.java
@@ -1,6 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package repasoexamen;
-
-import java.util.Scanner;
 
 /**
  *
@@ -12,15 +15,7 @@ public class RepasoExamen {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int val = sc.nextInt();
-        
-        if (val == 1){
-        
-            System.out.println("Mi moto al pina derrepente");
-        
-        }
+        // TODO code application logic here
     }
     
 }
