522baa622e5034ca70e5ed7910b06fb9e577d451
Enrique Alvarez <ealvarezpereira@danielcastelao.org>
2017-11-16 17:36:40 +0100

    Commit 2.

--- RepasoExamen.java
+++ RepasoExamen.java
@@ -1,9 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package repasoexamen;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +12,15 @@ public class RepasoExamen {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        
+        int val = sc.nextInt();
+        
+        if (val == 1){
+        
+            System.out.println("Mi moto al pina derrepente");
+        
+        }
     }
     
 }
